96e592c2fb5a3c99c8f8d155eb16c78b43c4c8d2
naiem <naiem4288@gamil.com>
2020-11-20 07:39:03 +0600

    add navbar hamburger menu icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 import Pricing from './components/Pricing/Pricing';
 import Subscribe from './components/Subscribe/Subscribe';
 import Footer from './components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Services from './Components/Services/Services';
+import Header from './components/Header/Header';
+import Services from './components/Services/Services';
 
 function App() {
   return (
