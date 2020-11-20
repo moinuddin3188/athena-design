28ada244d4591d148c2e7c8601bee368a36a8315
naiem <naiem4288@gamil.com>
2020-11-20 15:29:25 +0600

    some correction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-import Pricing from './Components/Pricing/Pricing';
-import Subscribe from './Components/Subscribe/Subscribe';
-import Footer from './Components/Footer/Footer';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import Header from './Components/Header/Header';
 import Services from './Components/Services/Services';
+import Pricing from './Components/Pricing/Pricing';
+import Subscribe from './Components/Subscribe/Subscribe';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
