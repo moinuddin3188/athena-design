c18f63c8767be41b27ca3ab48b9fce719f83e321
moinuddin3188 <moinuddin3188@gmail.com>
2020-11-20 10:09:15 +0600

    fooo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import Pricing from './components/Pricing/Pricing';
 import Subscribe from './components/Subscribe/Subscribe';
-import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Services from './components/Services/Services';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
